f02d3b2522a61260426634b34002e1613731fd87
toffeantyri <tofy3434@yandex.ru>
2022-07-25 17:02:10 +0500

    refactor: delete my Worker bookRepo: del code with workManager preparing build: depend add retrofit

--- app/src/main/java/ru/reader/viewpagermodule/busines/repository/LoadBookRepository.kt
+++ app/src/main/java/ru/reader/viewpagermodule/busines/repository/LoadBookRepository.kt
@@ -1,20 +1,8 @@
 package ru.reader.viewpagermodule.busines.repository
 
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import ru.reader.viewpagermodule.APP_CONTEXT
-
 class LoadBookRepository : BaseRepository<LoadingBookState>() {
 
     fun loadBook(listUrl: List<String>, onSuccess: () -> Unit, onFail: () -> Unit){
-        val constrains = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-        val downloadRequest =
-            OneTimeWorkRequestBuilder<DownloadBooksHelper>().setConstraints(constrains).build()
-
-        WorkManager.getInstance(APP_CONTEXT)
-            .enqueue(downloadRequest)
 
         onSuccess()
 
