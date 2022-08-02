2743f046cd8bb6784df375fa82d8f0ea61bf2357
toffeantyri <tofy3434@yandex.ru>
2022-08-02 12:16:27 +0600

    feat: add StorageHelper with empty fun saveFile and enum with state saving refactor: replayce UtilZip to StrorageHelper

--- app/src/main/java/ru/reader/viewpagermodule/data/busines/storage/StorageHelper.kt
+++ app/src/main/java/ru/reader/viewpagermodule/data/busines/storage/StorageHelper.kt
@@ -1,17 +1,29 @@
-package ru.reader.viewpagermodule.util
+package ru.reader.viewpagermodule.data.busines.storage
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.ResponseBody
+import ru.reader.viewpagermodule.App
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.util.zip.ZipFile
 
+class StorageHelper {
 
-object UtilZip {
+    suspend fun saveFileToPublicLocalPath(responseBody: ResponseBody, toWillFileName: String): StateSave {
+        withContext(Dispatchers.IO) {
 
-    private const val BUFFER_SIZE = 4096
+        }
 
-    fun unzipFile(zipFilePath: File, targetPath: String) {
+
+
+        return StateSave.STATE_SAVED
+    }
+
+
+    fun unzipFile(zipFilePath: File, targetPath: String, defaultNameFile: String) {
         File(targetPath).run {
             if (!exists()) {
                 mkdirs()
@@ -20,7 +32,7 @@ object UtilZip {
         ZipFile(zipFilePath).use { zip ->
             zip.entries().asSequence().forEach { entry ->
                 zip.getInputStream(entry).use { input ->
-                    val filePath = targetPath + "/" + entry.name
+                    val filePath = "$targetPath/$defaultNameFile"
                     if (!entry.isDirectory) {
                         // if the entry is a file, extracts it
                         extractFile(input, filePath)
@@ -36,7 +48,7 @@ object UtilZip {
 
     private fun extractFile(inputStream: InputStream, targetPathOut: String) {
         val bos = BufferedOutputStream(FileOutputStream(targetPathOut))
-        val bytesIn = ByteArray(BUFFER_SIZE)
+        val bytesIn = ByteArray(BUFFER_SIZE_ZIP)
         var read: Int
         while (inputStream.read(bytesIn).also { read = it } != -1) {
             bos.write(bytesIn, 0, read)
@@ -44,5 +56,15 @@ object UtilZip {
         bos.close()
     }
 
-}
 
+    companion object {
+        private const val BUFFER_SIZE_ZIP = 8096
+        val localPathFile = App.getMyPublicPath
+
+        enum class StateSave {
+            STATE_SAVED,
+            STATE_NOT_SAVED,
+            STATE_RESPONSE_BODY_NULL
+        }
+    }
+}
---
+++ app/src/main/java/ru/reader/viewpagermodule/data/busines/storage/UtilZip.kt
@@ -0,0 +1,9 @@
+package ru.reader.viewpagermodule.data.busines.storage
+
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.util.zip.ZipFile
+
+
