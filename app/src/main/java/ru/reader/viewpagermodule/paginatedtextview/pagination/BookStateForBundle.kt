7625ad8d9d4fdb38480f987d4894cab426c4880b
toffeantyri <tofy3434@yandex.ru>
2022-08-04 16:32:17 +0600

    test for random text is Success! Add data class for put in bundle for open in bookFragment feat : BookPagerFragment.kt : frag impl listeners listeners add to PagingTextView

---
+++ app/src/main/java/ru/reader/viewpagermodule/paginatedtextview/pagination/BookStateForBundle.kt
@@ -0,0 +1,11 @@
+package ru.reader.viewpagermodule.paginatedtextview.pagination
+
+import java.io.Serializable
+import java.nio.file.Path
+
+data class BookStateForBundle(
+    val bookName: String,
+    val tagName: String,
+    val absolutePath : String,
+    val readState: ReadState
+) : Serializable
--- app/src/main/java/ru/reader/viewpagermodule/view/screens/BookPagerFragment.kt
+++ app/src/main/java/ru/reader/viewpagermodule/view/screens/BookPagerFragment.kt
@@ -1,19 +1,30 @@
 package ru.reader.viewpagermodule.view.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_book_pager.*
+import kotlinx.android.synthetic.main.fragment_book_pager.view.*
 import ru.reader.viewpagermodule.R
+import ru.reader.viewpagermodule.paginatedtextview.pagination.ReadState
+import ru.reader.viewpagermodule.paginatedtextview.view.OnActionListener
+import ru.reader.viewpagermodule.paginatedtextview.view.OnSwipeListener
+import ru.reader.viewpagermodule.paginatedtextview.view.PaginatedTextView
+import java.io.InputStream
 
 
-class BookPagerFragment : Fragment() {
+class BookPagerFragment : Fragment(), OnSwipeListener, OnActionListener {
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = this
-    }
+    private lateinit var tvNameBook: TextView
+    private lateinit var tvPage: TextView
+    private lateinit var tvPercent: TextView
+
+    private lateinit var tvBookContent : PaginatedTextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +32,34 @@ class BookPagerFragment : Fragment() {
     ): View? {
         val view0 = inflater.inflate(R.layout.fragment_book_pager, container, false)
 
+
+        tvBookContent = view0.ptv_book_text
+        tvBookContent.setup(getText(resources.openRawResource(R.raw.sample_text)))
+        tvBookContent.setOnActionListener(this)
+        tvBookContent.setOnSwipeListener(this)
         return view0
+    }
+
+    override fun onSwipeLeft() {
+    }
+
+    override fun onSwipeRight() {
+    }
+
+    override fun onPageLoaded(state: ReadState) {
+    }
+
+    override fun onClick(paragraph: String) {
+    }
+
+    override fun onLongClick(word: String) {
+    }
+
+
+    private fun getText(inputStream: InputStream): String {
+        val bytes = ByteArray(inputStream.available())
+        inputStream.read(bytes)
+        return String(bytes)
     }
 
 }
