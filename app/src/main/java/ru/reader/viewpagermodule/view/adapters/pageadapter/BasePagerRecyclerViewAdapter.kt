e92b529997e05cbebfb618e3616b421409cb062f
toffeantyri <tofy3434@yandex.ru>
2022-08-10 17:03:58 +0600

    Refactor: adapter : add fun set controller

--- app/src/main/java/ru/reader/viewpagermodule/view/adapters/pageadapter/BasePagerRecyclerViewAdapter.kt
+++ app/src/main/java/ru/reader/viewpagermodule/view/adapters/pageadapter/BasePagerRecyclerViewAdapter.kt
@@ -1,10 +1,14 @@
 package ru.reader.viewpagermodule.view.adapters.pageadapter
 
+import android.text.TextPaint
 import androidx.recyclerview.widget.RecyclerView
+import ru.reader.viewpagermodule.paginatedtextview.pagination.PaginationController
 
-abstract class  BasePagerRecyclerViewAdapter<T : RecyclerView.ViewHolder>() : RecyclerView.Adapter<T>() {
+abstract class BasePagerRecyclerViewAdapter<T : RecyclerView.ViewHolder>() : RecyclerView.Adapter<T>() {
 
+    protected var controller: PaginationController? = null
+    protected var isMeasured = false
 
-
+//todo adapter extend this - need fun setTextPaint and setController
 
 }
--- app/src/main/java/ru/reader/viewpagermodule/view/adapters/pageadapter/ViewPagerAdapter.kt
+++ app/src/main/java/ru/reader/viewpagermodule/view/adapters/pageadapter/ViewPagerAdapter.kt
@@ -1,14 +1,17 @@
 package ru.reader.viewpagermodule.view.adapters.pageadapter
 
+import android.text.TextPaint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
 import ru.reader.viewpagermodule.R
+import ru.reader.viewpagermodule.paginatedtextview.pagination.PaginationController
 
 class ViewPagerAdapter : BasePagerRecyclerViewAdapter<ViewPagerAdapter.TextHolder>() {
 
@@ -20,6 +23,10 @@ class ViewPagerAdapter : BasePagerRecyclerViewAdapter<ViewPagerAdapter.TextHolde
         @BindView(R.id.item_bg)
         lateinit var bg: FrameLayout
 
+        @BindView(R.id.tv_text_page)
+        lateinit var text: TextView
+
+
         fun setData(color: Int) {
             bg.setBackgroundColor(ContextCompat.getColor(view.context, color))
         }
@@ -30,7 +37,8 @@ class ViewPagerAdapter : BasePagerRecyclerViewAdapter<ViewPagerAdapter.TextHolde
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TextHolder {
-        return TextHolder(LayoutInflater.from(p0.context).inflate(R.layout.custom_color_item, p0, false))
+        val view = TextHolder(LayoutInflater.from(p0.context).inflate(R.layout.custom_color_item, p0, false))
+        return view
     }
 
     override fun onBindViewHolder(holder: TextHolder, pos: Int) {
@@ -43,6 +51,19 @@ class ViewPagerAdapter : BasePagerRecyclerViewAdapter<ViewPagerAdapter.TextHolde
     fun updateAdapter(newList: List<Int>) {
         list.addAll(newList)
         notifyDataSetChanged()
+    }
+
+    fun setController(
+        fullText: CharSequence,
+        width: Int,
+        height: Int,
+        textPaint: TextPaint,
+        spacingMult: Float,
+        spacingExtra: Float
+    ) {
+        if (controller == null) {
+            controller = PaginationController(fullText, width, height, textPaint, spacingMult, spacingExtra)
+        }
     }
 
 
